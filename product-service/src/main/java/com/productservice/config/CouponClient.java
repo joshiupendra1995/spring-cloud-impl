91b050636e99143ba911f0098bafe35eb887803f
Upendra Joshi <joshiupendra34@gmail.com>
2023-11-02 10:10:37 +0530

    added cloud bus

--- product-service/src/main/java/com/productservice/config/CouponClient.java
+++ product-service/src/main/java/com/productservice/config/CouponClient.java
@@ -1,10 +1,12 @@
 package com.productservice.config;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient("cloud-gateway-service")
+//@RibbonClient("coupon-service")
 public interface CouponClient {
 
     @GetMapping("/v1/coupon/apply-coupon/{couponCode}")
